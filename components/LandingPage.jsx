55c22217684f3650c346811d0bc27f37a818a25a
Alex Tran <alextran8385@gmail.com>
2022-06-30 19:39:51 +1000

    styles and gradient

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import styles from "../styles/Sass/LandingPage.module.css"
 const LandingPage = () => {
     return (
-        <div>
+        <section className={styles.section}>
+            <div>
            <p>Hi There I&#39;m </p> 
            <h1>Alex Tran</h1>
             <h3>Self-Taught 
               <br></br>
             Full Stack Web Developer</h3>
             <p>Im a Computer Science Student with who is looking to specialize in web dev</p>
-        </div>
+            </div>
+        </section>
     );
 }
 
