b790cfbf8584949de56a2e5c45909858611df7e7
Alex Tran <alextran8385@gmail.com>
2022-06-30 18:28:40 +1000

    css files

---
+++ components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from "../styles/Sass/LandingPage.module.css"
+const LandingPage = () => {
+    return (
+        <div>
+           <p>Hi There I&#39;m </p> 
+           <h1>Alex Tran</h1>
+            <h3>Self-Taught 
+              <br></br>
+            Full Stack Web Developer</h3>
+            <p>Im a Computer Science Student with who is looking to specialize in web dev</p>
+        </div>
+    );
+}
+
+export default LandingPage;
