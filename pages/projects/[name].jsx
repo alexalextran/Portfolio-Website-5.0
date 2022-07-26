ad0d6246e9ee20bc9576ff9d97d75fd51e56fecb
Alex Tran <alextran8385@gmail.com>
2022-07-26 19:57:20 +1000

    more edits to the stack and links section to the dynamic page

--- pages/projects/[name].jsx
+++ pages/projects/[name].jsx
@@ -5,6 +5,10 @@ import ProjectPurpose from '../../components/UI/ProjectPurpose.jsx';
 import ProjectStack from '../../components/UI/ProjectStack.jsx';
 import ProjectIssues from '../../components/UI/ProjectIssues.jsx';
 import styles from "../../styles/Sass/ProjectsDynamic.module.css"
+import { BsGithub } from 'react-icons/bs';
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi'
+import Link from 'next/link.js';
+
 
 export const getStaticPaths = () => {
 
@@ -35,21 +39,34 @@ return{
 }
 
 const Name = ({project}) => {
- 
+ console.log(project.stack)
     return (
         <div className={styles.section}>
             <h1>{project.name}</h1>
             <p className={styles.desc}>{project.desc}</p>
          
                 <div className={styles.stackLink}>
-                    <div className={styles.stack}>
-                       <h4>Stack</h4>  
+                   
+                       <div className={styles.stack__array}>
+                       <h4>Stack</h4> 
+                       {
+                        (project.stack).map((stack, index) => 
+                        <p key={index}>{stack}</p>
+                        )
+                       }
+                       </div>
 
-                    </div>
+                
                     
                     <div className={styles.link}>
                        <h4>Links</h4> 
-                       
+                       <Link href={`${project.linkgit}`}className={styles.rightIcon +" "+ styles.github}>
+                        <BsGithub/>
+                       </Link>
+                       <Link href={`${project.linksite}`} className={styles.rightIcon +" "+ styles.linkedin}>
+                        <HiOutlineArrowNarrowRight/>
+                       </Link>
+
                     </div>
                     
                 </div>
--- public/data/projects.js
+++ public/data/projects.js
@@ -4,37 +4,58 @@ const projects = [
     {
         name: 'Max-Cozis',
         img: {MaxCozis},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa'
+        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
+        stack: ['HTML', 'CSS', 'JS'],
+        linkgit: 'https://github.com/alexalextran/react-adash',
+        linksite: 'https://alexalextran.github.io/memory-game-v2/'
     },
     {
         name: 'Memory Game V2',
         img: {MaxCozis},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa'
+        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
+        stack: ['HTML', 'CSS', 'JS'],
+        linkgit: 'https://github.com/alexalextran/react-adash',
+        linksite: 'https://alexalextran.github.io/memory-game-v2/'
     },
     {
         name: 'Memories',
         img: {MaxCozis},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa'
+        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
+        stack: ['HTML', 'CSS', 'JS'],
+        linkgit: 'https://github.com/alexalextran/react-adash',
+        linksite: 'https://alexalextran.github.io/memory-game-v2/'
     },
     {
         name: 'A- Academy',
         img: {MaxCozis},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa'
+        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
+        stack: ['HTML', 'CSS', 'JS'],
+        linkgit: 'https://github.com/alexalextran/react-adash',
+        linksite: 'https://alexalextran.github.io/memory-game-v2/'
     },
     {
         name: 'Notflix',
         img: {MaxCozis},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa'
+        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
+        stack: ['HTML', 'CSS', 'JS'],
+        linkgit: 'https://github.com/alexalextran/react-adash',
+        linksite: 'https://alexalextran.github.io/memory-game-v2/'
     },
     {
         name: 'E-Commerce Store',
         img: {MaxCozis},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa'
+        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
+        stack: ['HTML', 'CSS', 'JS'],
+        linkgit: 'https://github.com/alexalextran/react-adash',
+        linksite: 'https://alexalextran.github.io/memory-game-v2/'
     },
     {
         name: 'Java JSP Mock Store',
         img: {MaxCozis},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa'
+        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
+        stack: ['HTML', 'CSS', 'JS'],
+        linkgit: 'https://github.com/alexalextran/react-adash',
+        linksite: 'https://alexalextran.github.io/memory-game-v2/'
     },
    
 ]
