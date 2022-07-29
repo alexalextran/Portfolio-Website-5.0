1ab436a70fc780c9113e8691300010f3e47d79e7
Alex Tran <alextran8385@gmail.com>
2022-07-30 00:52:26 +1000

    removed and replaced alot of the data place holders for the dynamic project page including description, stack and links

--- public/data/projects.js
+++ public/data/projects.js
@@ -26,10 +26,10 @@ const projects = [
     {
         name: 'Max-Cozis',
         img: {MaxCozis},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
-        stack: ['HTML', 'CSS', 'JS'],
-        linkGit: 'https://github.com/alexalextran/react-adash',
-        linkSite: 'https://alexalextran.github.io/memory-game-v2/',
+        desc: 'A website built using firebase and react I comissioned for a friend intended to articulate his artistic talents, the website was intended to allow the client to self manage his artistic works by utilising CRUD functionality for all the works showcased on the site',
+        stack: ['Sass','JavaScript','React', 'Firebase'],
+        linkGit: 'https://github.com/alexalextran/max-cozis',
+        linkSite: 'https://alexalextran.github.io/max-cozis/',
         projectdata: {
             purpose: 'texadwadad awdha dwadd awdhgawdauyd agdad',
             purposeimg: {MaxCozis},
@@ -46,9 +46,9 @@ const projects = [
     {
         name: 'Memory Game V2',
         img: {MemoryGame},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
-        stack: ['HTML', 'CSS', 'JS'],
-        linkGit: 'https://github.com/alexalextran/react-adash',
+        desc: 'The second iteration of my original memory game which draws heavy inspiration from the human benchmark, it was built from the ground up using react, it was intended as a project to develop and strengthen my core skills on reacts fundamental concepts based around state, useEffect hooks and child component rendering. Its main feature is a live leaderboard that allows users to compare their highscores',
+        stack: ['Sass','JavaScript','React', 'Firebase'],
+        linkGit: 'https://github.com/alexalextran/memory-game-v2',
         linkSite: 'https://alexalextran.github.io/memory-game-v2/',
         projectdata: {
             purpose: 'texadwadad awdha dwadd awdhgawdauyd agdad',
@@ -65,10 +65,10 @@ const projects = [
     {
         name: 'Memories',
         img: {Memories},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
-        stack: ['HTML', 'CSS', 'JS'],
-        linkGit: 'https://github.com/alexalextran/react-adash',
-        linkSite: 'https://alexalextran.github.io/memory-game-v2/',
+        desc: 'My first ever MERN Stack project, which introduced me to key concepts such as nodejs, mongoDB and redux to manage and manipulate a database using both backend and frontend concepts',
+        stack: ['TailWind','JavaScript','React', 'MongoDB', 'NodeJs', 'ExpressJs'],
+        linkGit: 'https://github.com/alexalextran/Memories_MERN',
+        linkSite: 'https://alexalextran.github.io/Memories_MERN/',
         projectdata: {
             purpose: 'texadwadad awdha dwadd awdhgawdauyd agdad',
             purposeimg: {MaxCozis},
@@ -84,10 +84,10 @@ const projects = [
     {
         name: 'A- Academy',
         img: {Academy},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
-        stack: ['HTML', 'CSS', 'JS'],
+        desc: 'My first independent react project and coincidently my first real exposure with working with a client, this website was designed with user experience as a heavy focus, so concepts such as responsivity and accessibility were heavily focused on, the site also involved live data fetched from youtube api to deliver up to date videos and statistics',
+        stack: ['CSS', 'JavaScript', 'React'],
         linkGit: 'https://github.com/alexalextran/react-adash',
-        linkSite: 'https://alexalextran.github.io/memory-game-v2/',
+        linkSite: 'http://aadarshanujacademy.com/',
         projectdata: {
             purpose: 'texadwadad awdha dwadd awdhgawdauyd agdad',
             purposeimg: {MaxCozis},
@@ -103,10 +103,10 @@ const projects = [
     {
         name: 'Notflix',
         img: {Notflix2},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
-        stack: ['HTML', 'CSS', 'JS'],
-        linkGit: 'https://github.com/alexalextran/react-adash',
-        linkSite: 'https://alexalextran.github.io/memory-game-v2/',
+        desc: 'My first real experience at emulating another site, this project was designed with design as a priority over function, so the application still involved simple login, registration and movie api fetching features native to Netflix but overall the design process was very enjoyable, studying techniques that netflix employs to provide a well designed UI',
+        stack: ['Sass', 'JavaScript', 'React', 'Firebase'],
+        linkGit: 'https://github.com/alexalextran/notflix',
+        linkSite: 'https://alexalextran.github.io/notflix/',
         projectdata: {
             purpose: 'texadwadad awdha dwadd awdhgawdauyd agdad',
             purposeimg: {MaxCozis},
@@ -122,10 +122,10 @@ const projects = [
     {
         name: 'E-Commerce Store',
         img: {Ecommerce},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
-        stack: ['HTML', 'CSS', 'JS'],
-        linkGit: 'https://github.com/alexalextran/react-adash',
-        linkSite: 'https://alexalextran.github.io/memory-game-v2/',
+        desc: 'A react application that was developed with the assitance of a mentor, it was developed as a way to demonstrate key react concepts such as dynamic components, passing props to child components and manipulating objects',
+        stack: ['CSS', 'React'],
+        linkGit: 'https://github.com/alexalextran/react-ecommerce-practice',
+        linkSite: 'https://alexalextran.github.io/react-ecommerce-practice/',
         projectdata: {
             purpose: 'texadwadad awdha dwadd awdhgawdauyd agdad',
             purposeimg: {MaxCozis},
@@ -141,10 +141,10 @@ const projects = [
     {
         name: 'Java JSP Mock Store',
         img: {JSP},
-        desc: 'diowajdioad adpwa jdiopawjdioawjdioad opaid opaiwdad iopjwa opd apdoijwaiod aoidj aiopwd aiod jad jsaklda jwdlka w da daw doipawd aw da dawdj awiodadidja wodwad awoid jaoid jaiowd awi awoid jaiod aoid awoid jaiwjdawio dawio i aiod aoidd awda wd awaw waa',
-        stack: ['HTML', 'CSS', 'JS'],
-        linkGit: 'https://github.com/alexalextran/react-adash',
-        linkSite: 'https://alexalextran.github.io/memory-game-v2/',
+        desc: 'A full stack application developed within a team for a software development subject that involved using software development practices to plan and develop a full stack web application that allowed users and staff members to manage an online store. The application was built using html, css, java and sql to provide a fully working application that met CRUD requirements',
+        stack: ['HTML', 'CSS', 'Java', 'SQL'],
+        linkGit: 'https://github.com/alexalextran/Iotbay',
+        linkSite: 'https://github.com/alexalextran/Iotbay',
         projectdata: {
             purpose: 'texadwadad awdha dwadd awdhgawdauyd agdad',
             purposeimg: {MaxCozis},
