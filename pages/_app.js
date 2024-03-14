2a55c1bec30870dcb9fa11c811eb04f80ed8d764
Alex Tran <alextran8385@gmail.com>
2024-03-14 22:59:03 +1100

    fix?

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import { ParallaxProvider } from 'react-scroll-parallax';
-import { Analytics } from "@vercel/analytics/react"
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
