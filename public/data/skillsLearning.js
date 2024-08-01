666f159af382c7723f534973a3a86bd69f7099aa
Alex Tran <alextran8385@gmail.com>
2024-08-01 23:08:33 +1000

    a

--- public/data/skillsLearning.js
+++ public/data/skillsLearning.js
@@ -1,30 +1,32 @@
-import Node from '../skills/nodejs.svg';
-import Java from '../skills/java.svg';
-import Express from '../skills/express.svg';
-import MongoDB from '../skills/mongodb.svg';
-import SQL from '../skills/sql.svg';
+import Node from '../skills/nodejs.svg'
+import Java from '../skills/java.svg'
+import Express from '../skills/express.svg'
+import Redux from '../skills/redux.svg'
+import MongoDB from '../skills/mongodb.svg'
+import SQL from '../skills/sql.svg'
 
-const skillsLearning = [
-  {
-    name: 'Node',
-    img: Node
-  },
-  {
-    name: 'Java',
-    img: Java
-  },
-  {
-    name: 'Express',
-    img: Express
-  },
-  {
-    name: 'MongoDB',
-    img: MongoDB
-  },
-  {
-    name: 'SQL',
-    img: SQL
-  }
-];
+ const skillsLearning = [
+    {
+        name: 'Node',
+        img: {Node}
+    },
+    {
+        name: 'Java',
+        img: {Java}
 
+    },
+    {
+        name: 'Express',
+        img: {Express}
+    },
+    {
+        name: 'MongoDB',
+        img: {MongoDB}
+    },
+    {
+        name: 'SQL',
+        img:{SQL}
+    },
+   
+]
 export default skillsLearning;
--- public/data/skillsUse.js
+++ public/data/skillsUse.js
@@ -1,55 +1,56 @@
-import HTML from '../skills/html.png';
-import CSS from '../skills/css.svg';
-import SASS from '../skills/sass.svg';
-import Javascript from '../skills/js.svg';
-import React from '../skills/react.svg';
-import FireBase from '../skills/firebase.svg';
-import Nextjs from '../skills/nextjs.svg';
-import Git from '../skills/git.svg';
-import Redux from '../skills/redux.svg';
-import Typescript from '../skills/typescript.svg';
+import HTML from '../skills/html.png'
+import CSS from '../skills/css.svg'
+import SASS from '../skills/sass.svg'
+import Javascript from '../skills/js.svg'
+import React from '../skills/react.svg'
+import FireBase from '../skills/firebase.svg'
+import Nextjs from '../skills/nextjs.svg'
+import Git from '../skills/git.svg'
+import Redux from '../skills/redux.svg'
+import Typescript from '../skills/typescript.svg'
 
-const skillsUse = [
-  {
-    name: 'HTML',
-    img: HTML
-  },
-  {
-    name: 'CSS',
-    img: CSS
-  },
-  {
-    name: 'SASS',
-    img: SASS
-  },
-  {
-    name: 'Javascript',
-    img: Javascript
-  },
-  {
-    name: 'Typescript',
-    img: Typescript
-  },
-  {
-    name: 'Redux',
-    img: Redux
-  },
-  {
-    name: 'React',
-    img: React
-  },
-  {
-    name: 'FireBase',
-    img: FireBase
-  },
-  {
-    name: 'Nextjs',
-    img: Nextjs
-  },
-  {
-    name: 'Git',
-    img: Git
-  }
-];
+ const skillsUse = [
+    {
+        name: 'HTML',
+        img: {HTML}
+    },
+    {
+        name: 'CSS',
+        img: {CSS}
 
+    },
+    {
+        name: 'SASS',
+        img: {SASS}
+    },
+    {
+        name: 'Javascript',
+        img: {Javascript}
+    },
+    {
+        name: 'Typescript',
+        img: {Typescript}
+    },
+    {
+        name: 'Redux',
+        img: {Redux}
+    },
+    {
+        name: 'React',
+        img: {React}
+    },
+    {
+        name: 'FireBase',
+        img:{FireBase}
+    },
+    {
+        name: 'Nextjs',
+        img:{Nextjs}
+    },
+    {
+        name: 'Git',
+        img:{Git}
+    },
+    
+]
 export default skillsUse;
